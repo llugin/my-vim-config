5ab2473aa4e1a49f44a48a46006ee96ea94a8272
leszekl <leszek.lugin@intel.com>
2018-04-22 12:36:15 +0200

    init installation script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "Installing Pathogen..."
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo "Installing NerdTree..."
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+echo "Installing clang_complete..."
+git clone https://github.com/Rip-Rip/clang_complete.git ~/.vim/clang_complete
+cd ~/.vim/clang_complete
+cp -R * ..
+cd ..
+make
+
+test -f ~/.vimrc
+if [ $? == 0 ]
+then
+    echo "Backing up existing ~/.vimrc to .vimrc.bak..."
+    cp ~/.vimrc .vimrc.bak
+fi
+
+echo "Copying .vimrc..."
+cp .vimrc ~/.vimrc
