f0ae69b7ca64f4b462e51963f01f6ff3a8d9e6a5
Leszek Lugin <leszek.lugin@intel.com>
2018-04-27 16:29:34 +0200

    add syntastic installation to install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "Installing NerdTree..."
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
+echo "Installing syntastic"
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+~/.vim/bundle/syntastic
+
 echo "Installing clang_complete..."
 git clone https://github.com/Rip-Rip/clang_complete.git ~/.vim/clang_complete
 cd ~/.vim/clang_complete
